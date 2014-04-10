91a6a095b90e92e7452ff818fb66c8d1f91f7524
jagdeeparora86 <jagdeeparora86@gmail.com>
2014-04-10 16:59:54 -0700

    Update Program.cs

--- StacksAndQueues/StacksAndQueues/Program.cs
+++ StacksAndQueues/StacksAndQueues/Program.cs
@@ -4,7 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// Creating a stack functionality using resizeable array.
+// Creating a stack functionality using resizeable array and linked list.
+// Link list provides constant time solution even at worst case scenario but wastes time and space in linking.
+// On the other hand array saves space and over all time is fast.
+
+// better to use link list when need to manupalate data for each node faster then over all run time.
+// better to use array where space is the concern.
+
 
 namespace StacksAndQueues
 {
